ed83a9a2b5035f2755ab278fe9f30e809bb5db98
nvdung1 <59380404+nvdung1@users.noreply.github.com>
2021-09-21 15:46:44 +0700

    bottom navigation bar

---
+++ tlu_app/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'TLU Schedule',
+      home: Home(),
+    );
+  }
+}
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex =0;0
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Schedule Today') ,
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Today',
+            backgroundColor: Colors.blue
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.all_inbox),
+              label: 'All',
+              backgroundColor: Colors.red
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.note_add),
+              label: 'Add',
+              backgroundColor: Colors.blue
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Setting',
+              backgroundColor: Colors.blue
+          ),
+        ],
+        onTap: (index){
+          setState(() {
+            _currentIndex= index;
+          });
+          print(_currentIndex);
+        },
+      ),
+    );
+  }
+}
+
