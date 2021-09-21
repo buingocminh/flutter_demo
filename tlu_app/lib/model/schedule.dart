83118824c455d330db98a0fb017dc845476b70d0
unknown <htminhk@gmail.com>
2021-09-21 21:33:02 +0700

    aloasdn

--- tlu_app/lib/model/schedule.dart
+++ tlu_app/lib/model/schedule.dart
@@ -3,7 +3,6 @@ class Schedule {
   final room;
   final time;
   final StartDate;
-  final EndDate;
-  Schedule({this.name,this.room, this.time, this.StartDate, this.EndDate });
+  Schedule({this.name,this.room, this.time, this.StartDate});
 
 }
