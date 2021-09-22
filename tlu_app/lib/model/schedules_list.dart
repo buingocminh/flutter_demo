a93e1b06d14d3b9f6e6730cdeac56db8dbe1d074
unknown <htminhk@gmail.com>
2021-09-22 20:43:31 +0700

    2209

--- tlu_app/lib/model/schedules_list.dart
+++ tlu_app/lib/model/schedules_list.dart
@@ -1,13 +1,24 @@
-import './schedule.dart';
+import 'dart:convert';
 
+import './schedule.dart';
+import 'package:http/http.dart'as http;
 class Schedules_list {
   var _schedulesList = [];
 
   List get schedules_list {
     return _schedulesList.toList();
   }
+  static const url="https://testproject-htminhk-default-rtdb.asia-southeast1.firebasedatabase.app/Schedule.json";
+  Future<void> GetList() async {
 
-  void GetList() {
-
+  }
+  Future<void>AddSchedule(name,room,time,start,end, teacher )async{
+    http.post(Uri.parse(url),body: jsonEncode({
+      "name": name,
+      "room":room,
+      "start":start,
+      "end":end,
+      "teacher":teacher
+    }));
   }
 }
