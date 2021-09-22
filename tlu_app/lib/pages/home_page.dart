74755a06723ffb441e410bb21b2d59a4cc589b66
nvdung1 <59380404+nvdung1@users.noreply.github.com>
2021-09-22 19:46:45 +0700

    tách home page

---
+++ tlu_app/lib/pages/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex =0;
+  final tabs= [
+    const Center(child: Text('Today')),
+    const Center(child: Text('All')),
+    const Center(child: Text('Add')),
+    const Center(child: Text('Setting')),
+    const Center(child: Text('shit'))
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Schedule Today') ,
+      ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Today',
+              backgroundColor: Colors.blue
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.all_inbox),
+              label: 'All',
+              backgroundColor: Colors.red
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.note_add),
+              label: 'Add',
+              backgroundColor: Colors.blue
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Setting',
+              backgroundColor: Colors.blue
+          ),
+        ],
+        onTap: (index){
+          setState(() {
+            _currentIndex= index;
+          });
+        },
+      ),
+    );
+  }
+}
