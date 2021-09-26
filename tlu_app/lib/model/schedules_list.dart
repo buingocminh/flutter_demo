20dcc85265c5bc78500c0127fec4f5edb2f465ec
unknown <htminhk@gmail.com>
2021-09-26 19:33:12 +0700

    2609

--- tlu_app/lib/model/schedules_list.dart
+++ tlu_app/lib/model/schedules_list.dart
@@ -2,29 +2,35 @@ import 'dart:convert';
 import 'package:intl/intl.dart';
 import './schedule.dart';
 import 'package:http/http.dart' as http;
+
 class Schedules_list {
   List<Schedule> schedulesList = [];
+
   // List get schedules_list {
   //   return _schedulesList.toList();
   // }
 
-
-  List<Schedule> GetListfromday( String dayint){
+  List<Schedule> GetListfromday(String dayint) {
     List<Schedule> list = [];
     var day = DateTime.parse(dayint);
-    schedulesList.forEach((schedule) {
-      if (schedule.startDate.isBefore(day)&&schedule.endDate.isAfter(day))  list.add(schedule) ;
-    },);
+    schedulesList.forEach(
+      (schedule) {
+        if (schedule.startDate.isBefore(day) && schedule.endDate.isAfter(day))
+          list.add(schedule);
+      },
+    );
     print(list);
     return list;
   }
-  DateTime getfinalDay (){
-    DateTime finalDay= DateTime.parse("0000-00-00") ;
+
+  DateTime getfinalDay() {
+    DateTime finalDay = DateTime.parse("0000-00-00");
     schedulesList.forEach((element) {
-      if (element.endDate.isBefore(finalDay)) finalDay=element.endDate;
+      if (element.endDate.isBefore(finalDay)) finalDay = element.endDate;
     });
     return finalDay;
   }
+
   var url =
       "https://testproject-htminhk-default-rtdb.asia-southeast1.firebasedatabase.app/Schedule.json";
 
@@ -33,18 +39,19 @@ class Schedules_list {
     final datas = jsonDecode(res.body) as Map<String, dynamic>;
     final List<Schedule> list = [];
     datas.forEach((scheduleId, value) {
-      var schedule =Schedule(
+      var schedule = Schedule(
           nameSubject: value["nameSubject"],
           room: value["room"],
           timeBegin: value["timeBegin"],
           startDate: value["startDate"],
           endDate: value["endDate"],
           dayAction: value["dayAction"]);
-      schedule.id=scheduleId;
+      schedule.id = scheduleId;
       list.add(schedule);
     });
     schedulesList = list;
   }
+
   // void convert(){;
   //   print(DateFormat("dd/MM/yyyy").format(DateTime.parse("2020-10-10")));
   // }
@@ -55,7 +62,7 @@ class Schedules_list {
           "room": room,
           "startDate": start,
           "endDate": end,
-          "imeBegin":time,
+          "imeBegin": time,
           "dayAction": dayAction
         }));
     if (res.statusCode == 200)
